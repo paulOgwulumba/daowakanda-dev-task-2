e4e5179500fbbdcc150cf6b4666b62af966cc482
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-04-23 17:51:40 +0000

    Testing

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,8 +1,10 @@
 import algosdk, {
+  AtomicTransactionComposer,
   makeAssetTransferTxnWithSuggestedParamsFromObject,
   makePaymentTxnWithSuggestedParamsFromObject,
 } from "algosdk";
 import * as algokit from "@algorandfoundation/algokit-utils";
+import { MNEMONIC_KEY } from './constant';
 import { SMART_CONTRACT_ARC_32 } from "./client";
 import { AppClient } from "@algorandfoundation/algokit-utils/types/app-client";
 
@@ -27,13 +29,62 @@ async function loadAccount() {
   const client = await loadClient();
 
   // Do not push your mnemonic key to GitHub
-  const account = client.account.fromMnemonic();
+  const account = client.account.fromMnemonic(MNEMONIC_KEY);
 
   return account;
 }
 
 async function buyAsset() {
-  // Your logic goes here
+  const account = await loadAccount();
+  const client = await loadClient();
+
+  const appClient = new AppClient({
+    appId: BigInt(APP_ID),
+    appSpec: JSON.stringify(SMART_CONTRACT_ARC_32),
+    algorand: client,
+  });
+
+  const suggestedParams = await client.client.algod.getTransactionParams().do();
+
+  const assetOptinTxn = makeAssetTransferTxnWithSuggestedParamsFromObject({
+    amount: 0,
+    from: account.addr,
+    to: account.addr,
+    suggestedParams,
+    assetIndex: ASA_ID,
+  });
+
+  const fundTxn = makePaymentTxnWithSuggestedParamsFromObject({
+      amount: 2_000,
+      from: account.addr,
+      to: 'ZSHM55LX3DA4XDFBC7HHWHAAXQUEDTEM22ZQBG6WISOADFSRKS3NQW6WAY',
+      suggestedParams: {
+        ...suggestedParams,
+        fee: 2_000,
+        flatFee: true,
+      },
+  });
+
+  const atc = new algosdk.AtomicTransactionComposer();
+
+  atc.addTransaction({ txn: assetOptinTxn, signer: account.signer })
+
+  atc.addMethodCall({
+    appID: APP_ID,
+    sender: account.addr,
+    signer: account.signer,
+    method: appClient.getABIMethod('buyListing'),
+    appForeignAssets: [ASA_ID],
+    methodArgs: [{ txn: fundTxn, signer: account.signer }],
+    suggestedParams: {
+      ...suggestedParams,
+      fee: 2_000,
+      flatFee: true,
+    }
+  })
+
+  const response = await atc.execute(client.client.algod, 8);
+  console.log(response);
 }
 
 async function main() {
