e07684a6421b1b174d192cc865f95a8809951903
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-04-23 16:40:00 +0000

    Add APP ID and asset ID

--- client/src/index.ts
+++ client/src/index.ts
@@ -6,8 +6,8 @@ import * as algokit from "@algorandfoundation/algokit-utils";
 import { SMART_CONTRACT_ARC_32 } from "./client";
 import { AppClient } from "@algorandfoundation/algokit-utils/types/app-client";
 
-const APP_ID = 0;
-const ASA_ID = 0;
+const APP_ID = 2948568810;
+const ASA_ID = 2948546348;
 
 async function loadClient() {
   const client = algokit.AlgorandClient.fromConfig({
