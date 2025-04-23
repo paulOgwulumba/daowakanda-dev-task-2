c1aaed09f09c666df6e88a677fac7f3545d50a18
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-04-23 16:11:09 +0000

    Set up boilerplate

--- client/src/index.ts
+++ client/src/index.ts
@@ -6,13 +6,16 @@ import * as algokit from "@algorandfoundation/algokit-utils";
 import { SMART_CONTRACT_ARC_32 } from "./client";
 import { AppClient } from "@algorandfoundation/algokit-utils/types/app-client";
 
+const APP_ID = 0;
+const ASA_ID = 0;
+
 async function loadClient() {
   const client = algokit.AlgorandClient.fromConfig({
     algodConfig: {
-      server: "https://testnet-api.algonode.cloud",
+      server: "https://mainnet-api.algonode.cloud",
     },
     indexerConfig: {
-      server: "https://testnet-idx.algonode.cloud",
+      server: "https://mainnet-idx.algonode.cloud",
     },
   });
 
@@ -26,55 +29,14 @@ async function loadAccount() {
   return account;
 }
 
-async function deploySmartContract() {
-  const account = await loadAccount();
-  const client = await loadClient();
-
-  const appFactory = client.client.getAppFactory({
-    appSpec: JSON.stringify(SMART_CONTRACT_ARC_32),
-    defaultSender: account.addr,
-    defaultSigner: account.signer,
-  });
-
-  const response = await appFactory.send.create({
-    method: "createApplication",
-  });
-
-  return {
-    appId: response.result.appId,
-    appAddress: response.result.appAddress,
-  };
-}
-
-async function fundSmartContract(appAddress: string) {
-  const account = await loadAccount();
-  const client = await loadClient();
-
-  const suggestedParams = await client.client.algod.getTransactionParams().do();
-
-  const fundTxn = makePaymentTxnWithSuggestedParamsFromObject({
-    amount: 200_000,
-    from: account.addr,
-    to: appAddress,
-    suggestedParams,
-  });
-
-  const atc = new algosdk.AtomicTransactionComposer();
-
-  atc.addTransaction({ txn: fundTxn, signer: account.signer });
-
-  const response = await atc.execute(client.client.algod, 8);
-  console.log(response);
+async function buyAsset() {
+  // Your logic goes here
 }
 
 async function main() {
-  console.log("deploying...");
-  const { appAddress, appId } = await deploySmartContract();
-  console.log("deployment successful", appId, appAddress);
-
-  console.log("Funding...");
-  await fundSmartContract(appAddress);
-  console.log("Funding successful");
+  console.log("Buying asset");
+  await buyAsset()
+  console.log("Asset bought successfully");
 }
 
 main();
